e28c59850a9f8c8651aa488393057a6d8fbabedf
aryapdf <aryaagnipradana@gmail.com>
2025-10-23 23:15:03 +0700

    complete login feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import {Providers} from "@/components/Providers";
+
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -19,15 +21,13 @@ export const metadata: Metadata = {
   title: "Hiring Management Dashboard",
   description: "Manage job postings and applications efficiently",
 };
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: {children: React.ReactNode; }) {
   return (
     <html lang="en">
       <body className={`${nunito.variable} ${nunitoSans.variable}`}>
+      <Providers>
         {children}
+      </Providers>
       </body>
     </html>
   );
