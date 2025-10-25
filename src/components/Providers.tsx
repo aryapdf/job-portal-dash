824f13d53398e5d28ac13c2e30f3b62885f97686
aryapdf <aryaagnipradana@gmail.com>
2025-10-25 11:12:40 +0700

    fix provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,27 +1,9 @@
 "use client"
 
-import { Provider } from "react-redux";
-import { store } from "@/store";
-import AuthListener from "@/components/AuthListener";
-import ProtectedAuth from "@/components/ProtectedAuth";
+import { Provider } from "react-redux"
+import { store } from "@/store"
 import React from "react";
-import {useScreenListener} from "@/hooks/useScreenListener";
 
-function ScreenListenerWrapper({ children }: { children: React.ReactNode }) {
-    useScreenListener(); // Only called once here!
-    return <>{children}</>;
-}
-
-export function Providers({children}: {children: React.ReactNode; }) {
-    return (
-        <Provider store={store}>
-            <AuthListener>
-                <ScreenListenerWrapper>
-                    {/*<ProtectedAuth>*/}
-                        {children}
-                    {/*</ProtectedAuth>*/}
-                </ScreenListenerWrapper>
-            </AuthListener>
-        </Provider>
-    )
+export function Providers({ children }: { children: React.ReactNode }) {
+    return <Provider store={store}>{children}</Provider>
 }
